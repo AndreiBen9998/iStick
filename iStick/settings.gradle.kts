fcd31c5579ed78a47742d898b16d752bdd55c692
Andrei Benesch <andreibenesch98@gmail.com>
2025-03-14 18:58:15 +0200

    Actualizare majoră: repository-uri noi, navigare îmbunătățită, optimizări

--- iStick/settings.gradle.kts
+++ iStick/settings.gradle.kts
@@ -1,16 +1,9 @@
-// iStick/settings.gradle.kts
-rootProject.name = "iStick"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
-    // DO NOT add any plugin declarations here
 }
 
 dependencyResolutionManagement {
@@ -19,9 +12,8 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
     }
 }
 
+rootProject.name = "iStick"
 include(":composeApp")
